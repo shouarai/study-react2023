372542581c26b0e86e4c49d977c0545850c2454f
shouarai <shouarai@gmail.com>
2023-02-16 07:50:19 +0900

    footer、center部分をリファクタ

---
+++ src/app/components/center.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export function Center() {
+	return (
+		<div className={styles.center}>
+			<Image
+				className={styles.logo}
+				src='/next.svg'
+				alt='Next.js Logo'
+				width={180}
+				height={37}
+				priority
+			/>
+			<div className={styles.thirteen}>
+				<Image src='/thirteen.svg' alt='13' width={40} height={31} priority />
+			</div>
+		</div>
+	);
+}
